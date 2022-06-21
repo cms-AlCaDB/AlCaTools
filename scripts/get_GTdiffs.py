8f61541b7973a6999a14eb33562f7233bcf51f3b
malbouis <malbouis@users.noreply.github.com>
2022-06-21 13:14:43 -0300

    update unused keys and edit link for Tamás

--- scripts/get_GTdiffs.py
+++ scripts/get_GTdiffs.py
@@ -30,7 +30,7 @@ def getGTs():
     ## now get the value from each key of release_autoCond and my autoCond:
     for i in rel_autoCond.autoCond.keys():
         # do not consider the keys from the autoCondModifiers.py or old ones in autoCond.py
-        if "Fake" in str(i) or "0T" in str(i) or "ddd" in str(i) or "GRun" in str(i) or "FULL" in str(i) or "HIon" in str(i) or "PIon" in str(i) or "PRef" in str(i) or "_T" in str(i) or "upgradeP" in str(i) or "run2_hlt_hi" in str(i) or "_ppref" in str(i) or str(i) == "mc" or "run1_hlt_relval" in str(i) or "run1_data" in str(i) or str(i) == "startup" or "starthi" in str(i) or "com10" in str(i) or "hltonline" in str(i) or "upgrade2017" in str(i) or "upgrade2021" in str(i) or "upgrade2022" in str(i):
+        if "Fake" in str(i) or "0T" in str(i) or "ddd" in str(i) or "GRun" in str(i) or "FULL" in str(i) or "HIon" in str(i) or "PIon" in str(i) or "PRef" in str(i) or "_T" in str(i) or "upgradeP" in str(i) or "run2_hlt_hi" in str(i) or "_ppref" in str(i) or str(i) == "mc" or "run1_hlt_relval" in str(i) or "run1_data" in str(i) or str(i) == "startup" or "starthi" in str(i) or "com10" in str(i) or "hltonline" in str(i) or "upgrade2017" in str(i) or "upgrade2021" in str(i) or "upgrade2022" in str(i)  or "run3_mc_2022v11" in str(i) or "run3_hlt_2022v11" in str(i) or "run3_data_2022v11" in str(i):
             continue
         
         autoCond_compare[i] = [autoCond.autoCond[i], rel_autoCond.autoCond[i]]
@@ -53,7 +53,7 @@ def get_diff_links(autoCond_compare):
     for key,value in autoCond_compare.items():
         if value[0] != value[1]:
             print(f'**{key}**')
-            print(f'{conddbrowser_link}/{value[0]}/{value[1]}\n')
+            print(f'{conddbrowser_link}/{value[1]}/{value[0]}\n')
     
 def main():
     autoCond_dict = getGTs()
